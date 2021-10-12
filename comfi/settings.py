c01fb160f23a31324dacb098249ed20c56f6b391
flavafloyd <floydphuti911@gmail.com>
2021-10-12 20:33:29 +0200

    first commit

--- comfi/settings.py
+++ comfi/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k0!o66ae+40o2h!j(*xr5$s^dgbo!978ar&vkiv=l5f^w9e474'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
