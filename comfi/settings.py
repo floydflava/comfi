d15492956bc294e5e37ceef7b67ed2d98faa7371
flavafloyd <floydphuti911@gmail.com>
2021-10-15 12:18:45 +0200

    first commit

--- comfi/settings.py
+++ comfi/settings.py
@@ -67,16 +67,17 @@ WSGI_APPLICATION = 'comfi.wsgi.application'
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    
+    'postgresql': dj_database_url.config()
         
-    }
+    
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
