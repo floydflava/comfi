3d5fe713a54baa01e23511ebc716c0d533db471e
flavafloyd <floydphuti911@gmail.com>
2021-10-17 12:26:39 +0200

    initial commit

--- comfi/settings/base.py
+++ comfi/settings/base.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
 
     'cloudinary_storage',
     'cloudinary',
+    'allauth.socialaccount.providers.google',
 
 ]
 
@@ -53,6 +54,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "django.core.context_processors.request",
             ],
         },
     },
@@ -92,6 +94,7 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend'
 )
 SITE_ID = 1
+ACCOUNT_EMAIL_VERIFICATION = "none"
 LOGIN_REDIRECT_URL = '/'
 
 # CRISPY FORMS
