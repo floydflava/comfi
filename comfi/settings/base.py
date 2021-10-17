c013f2cc6587e91121aff75ec20227281f12e117
flavafloyd <floydphuti911@gmail.com>
2021-10-17 12:49:43 +0200

    initial commit

--- comfi/settings/base.py
+++ comfi/settings/base.py
@@ -94,7 +94,7 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend'
 )
 SITE_ID = 1
-# ACCOUNT_EMAIL_VERIFICATION = "none"
+ACCOUNT_EMAIL_VERIFICATION = "none"
 LOGIN_REDIRECT_URL = '/'
 
 # CRISPY FORMS
