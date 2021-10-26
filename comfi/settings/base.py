c50352a28df654773a46f594ad5e3ddbbd523e18
flavafloyd <floydphuti911@gmail.com>
2021-10-26 14:01:33 +0200

    google login added

--- comfi/settings/base.py
+++ comfi/settings/base.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'cloudinary',
     # 'allauth.socialaccount.providers.vk',  # if you need VK api
-    # 'allauth.socialaccount.providers.facebook',  # if you need FB api
+    'allauth.socialaccount.providers.facebook',  # if you need FB api
     'allauth.socialaccount.providers.google',
     
 
